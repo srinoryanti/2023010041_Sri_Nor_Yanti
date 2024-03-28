363636bb69850048884bd9e60bb631cf67f15412
sri nor yanti <yantisrinoor@gmail.com>
2024-03-28 10:10:05 +0700

    contoh1

--- koments.js
+++
@@ -1,7 +0,0 @@
-//consule.log("ini adalah komen satu baris");
-console.log("Tugas komen //");
-
-/*console.log("ini adalah komen lebih dari satu baris");
-console.log("Teks apapun yang berada disini maka akan dijadikan komen");
-console.log("ketika menggunakan ini, jangan sampai lupa menutupnya");*/
-console.log("Tugas komen /* */");
