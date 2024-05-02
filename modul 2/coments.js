091fff1933e2d4b959faa42a2f8ad5da0e71b7af
sri nor yanti <yantisrinoor@gmail.com>
2024-05-02 08:09:52 +0700

    Undefined

--- modul 2/coments.js
+++ modul 2/coments.js
@@ -4,4 +4,4 @@ console.log("Tugas komen //");
 /*console.log("ini adalah komen lebih dari satu baris");
 console.log("Teks apapun yang berada disini maka akan dijadikan komen");
 console.log("ketika menggunakan ini, jangan sampai lupa menutupnya");*/
-console.log("Tugas komen /* */");
+console.log("Tugas komen /*/");
